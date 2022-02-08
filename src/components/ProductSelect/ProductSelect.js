dfcd37d82619cdac2ca324f4a251ee5b409b5398
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2022-02-08 21:10:10 +0100

    select

--- src/components/ProductSelect/ProductSelect.js
+++ src/components/ProductSelect/ProductSelect.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import {StyledProductSelect,Select,Option} from "./ProductSelect.styled"
+import { StyledProductSelect, Select, Option } from "./ProductSelect.styled";
 import { useSelector, useDispatch } from "react-redux";
 import { displayItem } from "../../actions";
 
@@ -8,49 +8,54 @@ const ProductSelect = () => {
   const location = useLocation();
   const cat = location.pathname.split("/")[2];
   const [filters, setFilters] = useState({});
+  const [selected, setSelected] = useState("VARIETY PACK");
 
   const productsReducer = useSelector((state) => state.productsReducer);
 
-console.log(productsReducer);
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  // const item = productsReducer.find((product) => product.name === selected);
 
-
+ 
   const handleFilters = (e) => {
     const value = e.target.value;
-    setFilters({ 
+    setFilters({
       ...filters,
-      [e.target.name]:value
-    })
+      [e.target.name]: value,
+    });
+    setSelected({
+      [e.target.name]: value,
+    });
 
-    console.log(value);
-  }
+    dispatch(displayItem(selected));
+  };
 
   return (
-  <StyledProductSelect>
-          <Select name="name" onChange={handleFilters}>
+    <StyledProductSelect>
+      <Select name="name" onChange={handleFilters}>
+        <Option>VARIETY PACK</Option>
+        <Option>SALTED PEANUT</Option>
+        <Option>HAZELNUT CAFE</Option>
+        <Option>ALMOND HORCHATA</Option>
+        <Option>DOUBLE DARK MINT</Option>
+        <Option>BANANA PECAN</Option>
+      </Select>
 
-            <Option>VARIETY PACK</Option>
-            <Option>SALTED PEANUT</Option>
-            <Option>HAZELNUT CAFE</Option>
-            <Option>ALMOND HORCHATA</Option>
-            <Option>DOUBLE DARK MINT</Option>
-            <Option>BANANA PECAN</Option>
-          </Select>
+      <button onClick={() => dispatch(displayItem("VARIETY PACK"))}>
+        click
+      </button>
 
-          <button onClick={()=> dispatch(displayItem(1))}>click</button>
-
-          {productsReducer.currentItem ?  
-(<>
-<div>{productsReducer.id}</div>
-    <h1>{productsReducer.currentItem.name}</h1>
-    <img src={productsReducer.currentItem.img} alt={productsReducer.currentItem.name}/> 
-    <p>{productsReducer.currentItem.description}</p>
-</>
-    )
-    : ("select item " )
-    
-    }
-  </StyledProductSelect>)
+      {selected ? (
+        <>
+          <div>{selected.id}</div>
+          <h1>{selected.name}</h1>
+          <img src={selected.img} alt={selected.name} />
+          <p>{selected.description}</p>
+        </>
+      ) : (
+        "select item "
+      )}
+    </StyledProductSelect>
+  );
 };
 
 export default ProductSelect;
--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,15 +1,15 @@
 
 const productsReducer = ( state = [], action) => {
   console.log(state);
-    const item = state.find(
-      (product) => product.id === action.payload
-    );
+    // const item = (state.find)(
+    //   (product) => product.name === action.payload
+    // );
 
   
     switch(action.type) {
       case "DISPLAY_ITEM":
         return {
-          ...state, currentItem:item
+          ...state, currentItem:action.payload,
     
         }
   
