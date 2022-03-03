1a1634851fb8828178d29597a9a61d7de9900bcc
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2022-03-03 13:51:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
      <Provider store = { store } >
     <BrowserRouter>
-    basename={"/ecommerce-chunks"}
+    basename={"/ecommerce-chunks"} 
       <App />
     </BrowserRouter>
      </Provider>
